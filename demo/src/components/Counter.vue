b2a6572d2e729e5211ca86d8b535e57e73fe8413
Alizwell <844534780@qq.com>
2018-08-05 22:15:20 +0800

    a  simple  vuex  demo

---
+++ demo/src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-on:click="increment">
+      {{ count }} : {{ id }} : {{ name }} : {{todo}}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations } from "Vuex";
+export default {
+  computed: {
+    count() {
+      return this.$store.state.count;
+    },
+    ...mapState({
+      id: "id",
+      name: "name"
+    }),
+    ...mapGetters({
+      todo: "doneTodos"
+    })
+  },
+  methods: {
+    // increment() {
+    //   this.$store.commit("increment");
+    // }
+    ...mapMutations(["increment"])
+  }
+};
+</script>
+
+
+<style lang="css">
+</style>
---
+++ demo/src/main.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import './css/style.css'
+import Counter from './components/Counter.vue'
+// import store from './store/index.js'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 11,
+    id: 22,
+    name: 'xuey',
+    todos: [{
+        id: 1,
+        text: '...',
+        done: true
+      },
+      {
+        id: 2,
+        text: '...',
+        done: false
+      }
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    }
+  },
+  mutations: {
+    increment(state) {
+      return state.count++;
+    }
+  }
+})
+
+// const Counter = {
+//   template: `
+//     <div  v-on:click="increment" >{{ count }}</div>
+//   `,
+//   computed: {
+//     count() {
+//       return this.$store.state.count
+//     }
+//   },
+//   methods: {
+//     increment() {
+//       this.$store.commit('increment')
+//     }
+//   }
+// }
+
+new Vue({
+  el: '#app',
+  store,
+  template: `
+  <Counter></Counter>
+  `,
+  components: {
+    Counter
+  }
+})
