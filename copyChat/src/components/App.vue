38e83a0bef8b46afb882e757617b50d1d5f25c4c
Alizwell <844534780@qq.com>
2018-08-09 10:50:23 +0800

    rename  copy  to  copyShop  and  new  folder  copyChat

---
+++ copyChat/src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Shopping  Cart Example</h1>
+        <hr>
+        <h2>Products</h2>
+        <Products/>         
+        <hr>
+        <ShopCart/>
+    </div>
+</template>
+
+<script>
+import Products from './Products.vue'
+import ShopCart from './ShopCart.vue'
+
+export  default {
+    components:{ Products, ShopCart }
+}
+
+</script>
+
+
---
+++ copyChat/webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const  ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+
+let config = {
+    entry: './src/app.js',
+    output: {
+        // path: path.resolve(__dirname, 'dist'),
+        path: path.join(__dirname, '__build__'),
+        // filename: 'bundle.js',
+        filename: '[name].js',
+        // publicPath: '/__build__/'
+    },
+    devtool: 'source-map',
+    devServer: {
+        inline: true,
+        hot: true,
+        host: '0.0.0.0',
+        port: 9001,
+        contentBase: path.resolve(__dirname, '__build__'),
+        compress: true
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+                // use: ExtractTextPlugin.extract({
+                //     fallback: 'style-loader',
+                //     use: 'css-loader'
+                // })
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        outputPath: './images/',
+                        limit: 500,
+                        publicPath: '../images'
+                    }
+                }]
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html'
+        }),
+        // new ExtractTextPlugin('css/[name].[hash:8].css'),
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
+    }
+};
+
+module.exports = config;
